6736e77e55a08d25585fe13280d2dd91940b5f2d
Prof: Marcelo <115180937+martematica@users.noreply.github.com>
2026-09-02 21:40:51 -0300

    Add files via upload

---
+++ javascript/empty-example/fluxograma.js
@@ -0,0 +1,105 @@
+function setup() {
+  createCanvas(1500, 800);
+}
+function draw() {
+  background(220);
+
+
+  line(130,400,750,200);
+  line(380,400,750,200);
+  line(630,400,750,200);
+  line(890,400,750,200);
+  line(1120,400,750,200);
+  line(1380,400,750,200);
+  line(630,400,750,200);
+
+  line(120,400,50,600);
+  line(120,400,180,600);
+  line(120,400,110,730);
+
+  line(370,400,280,600);
+  line(370,400,440,600);
+  line(370,400,350,730);
+  
+  line(630,400,540,600);
+  line(630,400,700,600);
+  line(630,400,620,730);
+
+  line(870,400,800,600);
+  line(870,400,950,600);
+
+  line(1130,400,1050,600);
+  line(1130,400,1130,730);
+  line(1130,400,1200,600);
+
+  line(1380,400,1300,600);
+  line(1380,400,1380,730);
+  line(1380,400,1450,600);
+
+  fill(0,100,200);
+  rect(600,100,300,150,20);
+  rect(50,400,150,100,20);
+  rect(300,400,150,100,20);
+  rect(550,400,150,100,20);
+  rect(800,400,150,100,20);
+  rect(1050,400,150,100,20);
+  rect(1300,400,150,100,20);
+
+
+circle(50,600,100);
+circle(110,730,100);
+circle(180,600,100);
+circle(280,600,100);
+circle(350,730,100);
+circle(440,600,100);
+circle(540,600,100);
+circle(620,730,100);
+circle(700,600,100);
+circle(800,600,100);
+circle(950,600,100);
+circle(1050,600,100);
+circle(1130,730,100);
+circle(1200,600,100);
+circle(1300,600,100);
+circle(1380,730,100);
+circle(1450,600,100);
+
+
+
+textSize(40);
+fill(250,250,250);
+text('Index.html',650,190);
+textSize(20);
+text('Física.html',70,460);
+text('Matematica.html',300,460);
+text('RoboticaEF.html',550,460);
+text('RoboticaEM.html',800,460);
+text('Programase',1050,450);
+text('plataformas.html',1050,470);
+text('horário.html',1310,460);
+
+textSize(15);
+text('1° ano.html',10,600);
+text('2° ano.html',70,740);
+text('3° ano.html',140,600);
+text('2° ano.html',310,740);
+text('1° ano.html',240,600);
+text('3° ano.html',400,600);
+text('1° ano.html',500,600);
+text('2° ano.html',580,740);
+text('      2° ano \n avancado.html',650,590);
+
+
+text('1° ano.html',760,600);
+text('2° ano.html',910,600);
+
+text('p5.js',1040,600);
+text('Visual \n studio',1100,730);
+text('Github',1180,600);
+text('Manhã',1280,600);
+text('Tarde',1360,730);
+text('Noite',1430,600);
+
+text('');
+
+}
